0fdcad8bbf6f47a9d3ffd42a683da6a8eca8f5e5
Brent Vatne <brentvatne@gmail.com>
2020-09-13 12:16:07 -0700

    example

--- App.js
+++ App.js
@@ -1,12 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from "react";
+import { Text, View, StyleSheet, Button } from "react-native";
+import Constants from "expo-constants";
+
+import * as FileSystem from "expo-file-system";
+import { useEffect, useRef, useState } from "react";
 
 export default function App() {
+  const [downloadProgress, setDownloadProgress] = useState(0);
+
+  const downloadResumable = FileSystem.createDownloadResumable(
+    "https://tse2-mm.cn.bing.net/th/id/OIP.YoNtXYr0k-kJBQNCKsPgWAHaEL?pid=Api&rs=1",
+    FileSystem.documentDirectory + "test.apk",
+    {},
+    (downloadProgress: {
+      totalBytesWritten: number,
+      totalBytesExpectedToWrite: number,
+    }) => {
+      console.log("download progress called");
+      const progress =
+        downloadProgress.totalBytesWritten /
+        downloadProgress.totalBytesExpectedToWrite;
+      console.log("total " + downloadProgress.totalBytesExpectedToWrite);
+      console.log("progress " + downloadProgress.totalBytesWritten);
+      setDownloadProgress(progress);
+    }
+  );
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.paragraph}>{downloadProgress}</Text>
+      <Button onPress={() => downloadResumable.downloadAsync()} title="Start Download" color="#841584" />
     </View>
   );
 }
@@ -14,8 +37,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: "#ecf0f1",
+    padding: 8,
+  },
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
+    fontWeight: "bold",
+    textAlign: "center",
   },
 });
